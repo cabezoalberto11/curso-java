513de24d22374c4b2deab416438b9784d321fdb7
cabezoalberto11 <168017168+cabezoalberto11@users.noreply.github.com>
2024-05-01 01:45:45 +0200

    Update ejercicio7.java
    Format

--- src/ejercicios/ejercicio7.java
+++ src/ejercicios/ejercicio7.java
@@ -1,7 +1,6 @@
 package ejercicios;
 import java.util.Scanner;
 public class ejercicio7 {
-
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
 		System.out.println("Introduce tu edad, por favor");
@@ -9,15 +8,10 @@ public class ejercicio7 {
 		
 		if(edad <4) {
 			System.out.println("ACCESO GRATUITO");
-		}
-		  else if(edad >=4 && edad <18) {
+		} else if(edad >=4 && edad <18) {
 			System.out.println("DEBES PAGAR 5 EUROS");
-		  }
-		    else {
+		} else {
 			System.out.println("DEBES PAGAR 10 EUROS");
-		    }
-		
-		
+		}
 	}
-
 }
